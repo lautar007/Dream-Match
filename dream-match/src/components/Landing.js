b52fffe9e840f51ae94f91111173c3ec2fa6b29a
lautaro <kautarol@gmail.com>
2022-04-02 14:09:26 -0300

    Landing terminado

--- dream-match/src/components/Landing.js
+++ dream-match/src/components/Landing.js
@@ -1,10 +1,32 @@
 import React from "react";
+import './styles/Landing.css';
+import ball from './media/ball.png';
+import CurArr1 from './media/curve arrow 1.png';
+import ex from './media/ex.png';
+import { Link } from "react-router-dom";
 
 export default function Landing(){
 
     return(
-        <div>
-            <h1>Este es el landing</h1>
+        <div className="fondo">
+            <div className="content">
+                <div className="border">
+                    <img className="ball" src={ball}/>
+                    <img className="curArr1" src={CurArr1}/>
+                    <img className="curArr2" src={CurArr1}/>
+                    <img className="ex" src={ex}/>
+                    <h1 className="titulo">Dream Teams</h1>
+                    <div className="cuadro">
+                    <h2 className="subtitulo">EL PARTIDO DE TUS SUEÑOS</h2>
+                    <h2 className="text">Crea dos equipos de fútbol con tus jugadores favoritos</h2>
+                </div>
+                <Link to='/teams'>
+                <button className="enter">Entrar</button>
+                </Link>
+            </div>
+        </div>
+            <div className="img">
+            </div>
         </div>
     )
 }
